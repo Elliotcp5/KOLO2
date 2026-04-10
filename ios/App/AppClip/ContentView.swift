e4e6126a0bd3b80d880539173267bfd1bc630a7b
Elliotcp5 <159551193+Elliotcp5@users.noreply.github.com>
2026-04-10 23:05:48 +0200

    Update ContentView.swift

--- ios/App/AppClip/ContentView.swift
+++ ios/App/AppClip/ContentView.swift
@@ -1,1 +1,16 @@
+import SwiftUI
 
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "house.fill")
+                .imageScale(.large)
+                .foregroundColor(.purple)
+            Text("KOLO")
+                .font(.largeTitle)
+                .bold()
+            Text("AI Real Estate")
+                .foregroundColor(.gray)
+        }
+    }
+}
